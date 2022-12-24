b328bc80385449431c856212f2f14e6646c9e0d0
alvaro <arecabarren14@alumnos.utalca.cl>
2022-12-24 15:05:29 -0300

    Add code for playRound function and test cases

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -22,15 +22,48 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     // Take player selection and make it case-insensitive
+    playerSelection = playerSelection.toLowerCase()
     // Define winner
     // Compare playerSelection with computer Selection
-    // if player choose rock:
-        //computerSelection is paper ? player wins
-        //else computer wins
-    // if player choose paper
-        //computerSelection is rock? player wins
-        //else computer wins
-    // if player choose scissors
-        //computerSelection is paper? player wins
-        //else computer wins
-  }
+    switch (playerSelection) {
+        case computerSelection.toLowerCase()://If are equals then ties!
+            return `It's a draw ${playerSelection} ties ${computerSelection}`;
+            break;
+
+        case "rock": // if player choose rock
+            //computerSelection is paper? player wins
+            if (computerSelection == 'Scissors') {
+                return `You win! ${playerSelection} beats ${computerSelection}`;
+                break;
+            } else {return `You lose! ${computerSelection} beats ${playerSelection}`};
+            break;
+        case "paper": // if player choose paper
+            if (computerSelection == 'Rock') {
+                return `You win! ${playerSelection} beats ${computerSelection}`;
+                break;
+            } else {return `You lose! ${computerSelection} beats ${playerSelection}`};
+            break;
+        case "scissors": // if player choose scissors
+            if (computerSelection == 'Paper') {
+                return `You win! ${playerSelection} beats ${computerSelection}`;
+                break;
+            } else {
+                return `You lose! ${computerSelection} beats ${playerSelection}`
+            };
+            break;
+        default:
+            break;
+    }
+}       
+//Testing winning cases
+console.log(playRound("Rock","Scissors"));
+console.log(playRound("Paper","Rock"));
+console.log(playRound("Scissors","Paper"));
+//Testing loser cases
+console.log(playRound("Rock","Paper"));
+console.log(playRound("Paper","Scissors"));
+console.log(playRound("Scissors","Rock"));
+//Testing ties
+console.log(playRound("Scissors","Scissors"));
+console.log(playRound("Rock","Rock"));
+console.log(playRound("Paper","Paper"));
