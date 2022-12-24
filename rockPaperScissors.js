347b9550dd282a2321d97ccdb727f4cfa9b80f73
alvaro <arecabarren14@alumnos.utalca.cl>
2022-12-24 18:35:42 -0300

    Add game() function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -55,15 +55,48 @@ function playRound(playerSelection, computerSelection) {
             break;
     }
 }       
-//Testing winning cases
-console.log(playRound("Rock","Scissors"));
-console.log(playRound("Paper","Rock"));
-console.log(playRound("Scissors","Paper"));
-//Testing loser cases
-console.log(playRound("Rock","Paper"));
-console.log(playRound("Paper","Scissors"));
-console.log(playRound("Scissors","Rock"));
-//Testing ties
-console.log(playRound("Scissors","Scissors"));
-console.log(playRound("Rock","Rock"));
-console.log(playRound("Paper","Paper"));
+// //Testing winning cases
+// console.log(playRound("Rock","Scissors"));
+// console.log(playRound("Paper","Rock"));
+// console.log(playRound("Scissors","Paper"));
+// //Testing loser cases
+// console.log(playRound("Rock","Paper"));
+// console.log(playRound("Paper","Scissors"));
+// console.log(playRound("Scissors","Rock"));
+// //Testing ties
+// console.log(playRound("Scissors","Scissors"));
+// console.log(playRound("Rock","Rock"));
+// console.log(playRound("Paper","Paper"));
+
+function game() {
+    
+    let playerRecord = 0, 
+        computerRecord = 0;
+    for (let i = 0; i < 5; i++) {
+        //Get player weapons
+        let playerSelection = prompt("Choose your weapons: ").toLowerCase();
+        //Get computer choise
+        let computerChoice = getComputerChoice()
+        //Check the result
+        let result = playRound(playerSelection,computerChoice)
+        
+        //Register the points
+        if(result == `You win! ${playerSelection} beats ${computerChoice}`){
+            playerRecord += 1;
+        } else if (result == `You lose! ${computerChoice} beats ${playerSelection}`){
+            computerRecord += 1;
+        }
+        //Display the result
+        console.log(result);
+        console.log(`You ${playerRecord} points`);
+        console.log(`Computer ${computerRecord} points`);
+    }
+    // Advise the winner
+    if (computerRecord > playerRecord) {
+        console.log('You lost')
+    } else if (computerRecord < playerRecord){
+        console.log('You won')
+    } else {
+        console.log("It's a tie!")
+    }
+}
